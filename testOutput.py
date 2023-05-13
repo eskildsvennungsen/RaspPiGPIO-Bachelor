db2f9a97e6b02be8759265fb2e766209daafa1a2
krso1002 <krstsolh@gmail.com>
2023-05-13 12:32:58 +0000

    Testet for-løkke og sensor

--- testOutput.py
+++ testOutput.py
@@ -1,3 +1,4 @@
+from periphery import GPIO
 import multiprocessing
 import time
 
@@ -6,7 +7,8 @@ import time
 ###########'##################
 #GPIO.setup(pinNum, "out")
 #GPIO.setup(pinNum, "out")
-#GPIO.setup(156, "in") 
+sensor = GPIO(156, "in")
+sensorValue = False
 
 pins = [1, 2, 3]
 delays = [1, 2, 3]
@@ -15,10 +17,14 @@ def toggle_pin(pin_num, delay):
     while True:
         time.sleep(delay)
         print(f'Prosess {pin_num}')
+        return
 
 if __name__ == '__main__':
-    for i in range(len(pins)):
-        prosess = multiprocessing.Process(target=toggle_pin, args=(pins[i], delays[i]))
-        prosess.start()
-    prosess.join()
+    while True:
+        if sensor.read() != sensorValue:
+            for i in range(len(pins)):
+                prosess = multiprocessing.Process(target=toggle_pin, args=(pins[i], delays[i]))
+                prosess.start()
+            prosess.join()
+        
     #print(len(pins))
